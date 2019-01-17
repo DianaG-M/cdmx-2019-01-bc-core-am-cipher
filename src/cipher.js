35de9b898ba28e3d706f5b6509ab207f9fa81da4
Diana <aries_di@hotmail.com>
2019-01-17 11:19:03 -0600

    Ya corren todos los test, ya codifica y decodifica

--- src/cipher.js
+++ src/cipher.js
@@ -1,40 +1,48 @@
-let nuevoCifrado = "";
+
+
 
 window.cipher = {
   encode : (offset,str) =>{
-    let mensajeObtenido = str.value;
-    let mayuscula = mensajeObtenido.toUpperCase();
-    offset = offset.value;
-    offset = Number(offset);
+    let nuevoCifrado = "";
+
+    let mayuscula = str.toUpperCase(); // convierte el mensaje a mayuscula   
+    offset = Number(offset);//convierte el valor en string a entero
     
+    //recorre los valores del mensaje que ingreso el usuario letra por letra
     for(let i = 0; i<mayuscula.length; i++){
+      //convierte cada letra a su valor en Ascii
       let letraAscii = mayuscula.charCodeAt(i);
+      //Fórmula para hacer el cifrado de acuerdo al offset que elige el usuario
       let formula = (letraAscii-65+offset)%26+65;
+      //Conviente el cifrado en Ascii a Alfabeto
       let resultado = String.fromCharCode(formula);
+      //Muestra el mensaje ya cifrado en forma de cadena
       nuevoCifrado+=resultado;
   
     }
-
-    alert(nuevoCifrado);
-
+    return nuevoCifrado;
   },
   
   decode : (offset,str) => {
-    let mensajeObtenido = str.value;
-    let mayuscula = mensajeObtenido.toUpperCase();
-    offset = offset.value;
-    offset = Number(offset);
+    let nuevodesCifrado = "";
     
+    let mayuscula = str.toUpperCase(); // convierte el mensaje a mayuscula
+    offset = Number(offset);//convierte el valor en string a entero
+    
+    //recorre los valores del mensaje que ingreso el usuario letra por letra
     for(let i = 0; i<mayuscula.length; i++){
+      //convierte cada letra a su valor en Ascii
       let letraAscii = mayuscula.charCodeAt(i);
+      //Fórmula para hacer el cifrado de acuerdo al offset que elige el usuario
       let formula = (letraAscii+65-offset)%26+65;
+      //Conviente el cifrado en Ascii a Alfabeto
       let resultado = String.fromCharCode(formula);
-      nuevoCifrado+=resultado;
+      //Muestra el mensaje ya cifrado en forma de cadena
+      nuevodesCifrado+=resultado;
   
     }
-
-    alert(nuevoCifrado);
+    return nuevodesCifrado;
   }
 
-  
+
 };
--- src/index.js
+++ src/index.js
@@ -11,24 +11,29 @@ const box2 = document.getElementById("box2");
 const box3 = document.getElementById("box3");
 
 //Variables que se ocupan en cipher.js para codificar y decodificar
-const str = document.getElementById("txt");
-const offset = document.getElementById("opcion");
-
-
+//str almacena el mensaje que el usuario ingresa    
+const str1 = document.getElementById("txt");
+//offset almancena el numero que el usuario elige
+const offset1 = document.getElementById("opcion");
 
 /*Funcion en ES6 que al momento de hacer click en el boton empezar oculta la seccion de bienvenida 
 y aparece la seccion box1*/
 comenzar.addEventListener("click", () => {
     bienvenida.classList.add("ocultar");
     box1.classList.remove("ocultar");
+    
 });
 
 /*Funcion en ES6 que al momento de hacer click en el boton codificar oculta la seccion de box1
 y aparece la seccion box2*/
 cifrar.addEventListener("click", () => {
+    
     box1.classList.add("ocultar");
     box2.classList.remove("ocultar");
-    window.cipher.encode(offset,str);
+    const str = str1.value;
+    const offset = offset1.value;
+    const union = window.cipher.encode(offset,str);
+    document.getElementById("mensajecifrado").innerHTML = union;
     
 });
 
@@ -37,7 +42,10 @@ y aparece la seccion box3*/
 descifrar.addEventListener("click", () => {
     box1.classList.add("ocultar");
     box3.classList.remove("ocultar");
-    window.cipher.decode(offset,str);
+    const str = str1.value;
+    const offset = offset1.value;
+    const union = window.cipher.decode(offset,str);
+    document.getElementById("mensajedescifrado").innerHTML = union;
 });
 
 /*Funcion en ES6 que al momento de hacer click en el boton regersar oculta la seccion de box2
